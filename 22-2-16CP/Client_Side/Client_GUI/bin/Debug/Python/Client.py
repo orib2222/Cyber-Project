8c83819277e95db1183369521068516504b19d43
orib2222 <orib2222@gmail.com>
2016-02-22 15:38:54 +0200

    22-2-16 version
    hello michael!

---
+++ 22-2-16CP/Client_Side/Client_GUI/bin/Debug/Python/Client.py
@@ -0,0 +1,83 @@
+
+
+import socket
+from Connection_Module import *
+from Detects_Process_Using_Camera import *
+import os
+
+def Checks_If_Camera_Is_Used():
+    print "Inside "
+    Detects_Process_Using_Camera_Obj = DetectProcess()
+    Detects_Process_Using_Camera_Obj.run()
+    Process_Name_Using_Camera =  Detects_Process_Using_Camera_Obj.process_name
+    if Process_Name_Using_Camera != None:
+        print "Inside " + Process_Name_Using_Camera
+    return Process_Name_Using_Camera
+
+def If_Process_Is_Not_Dangerous(Process_Name_Using_Camera):
+    return False
+
+if __name__=='__main__':
+
+    Local_Obj_Pipe = Connecting_Using_Pipe()
+
+    Data_From_Server = ""
+    Data_From_Server = Local_Obj_Pipe.Pipe_Server_To_Client()
+
+    print "Data_From_Server: " + Data_From_Server
+
+    Temp_Arr = Data_From_Server.split(',')
+
+    if Temp_Arr[0] == "ori" and Temp_Arr[1] == "123":
+        print "OK"
+        Data_From_Client = "OK"
+        Local_Obj_Pipe.Pipe_Client_To_Server(Data_From_Client)
+
+    else:
+        Data_From_Client = "NOTOK"
+        Local_Obj_Pipe.Pipe_Client_To_Server(Data_From_Client)
+
+
+
+    Socket_Obj = Connecting_Using_Socket()
+    ##my_socket = Socket_Obj.Creating_Socket()
+    my_socket = socket.socket()
+    my_socket.connect(('192.168.56.1', 8084))
+
+    User_Name_And_Password = Data_From_Server
+    Socket_Obj.Socket_Send(my_socket, Data_From_Server)
+
+    ##print "AFTER"
+    while True:
+        Process_Name_Using_Camera = Checks_If_Camera_Is_Used() ## And Returns The Process Name Using Camera
+        ##print "Hello1"
+        if Process_Name_Using_Camera == "" or Process_Name_Using_Camera == None: ## Checks If Camera Process Is Used
+            break
+        ##print "Hello2"
+        bool = If_Process_Is_Not_Dangerous(Process_Name_Using_Camera) ## Returns False If The Parameter Process Is Dangerous
+        ##print "bool: " + str(bool)
+        if bool == False:  ##if Camera is Used By a Hurting Program Returns True
+            test = os.system("taskkill /im " + Process_Name_Using_Camera)
+            if test == 128: ## If Process Not Exists
+                print "No Such Process"
+
+
+
+
+            Data_To_Send="KILLED PROCESS USING CAMERA " + Process_Name_Using_Camera
+
+            t2 = Thread(target=Socket_Obj.Socket_Send, args=(my_socket,Data_To_Send))
+            t2.start()
+
+            print "DTS " + Data_To_Send
+
+            data = Socket_Obj.Socket_Recieve(my_socket)
+
+            if data != "":
+                print data
+                my_socket.close()
+
+            break
+    while True:
+        continue
+    print "Out Of While"
---
+++ 22-2-16CP/Server_Side/Server_GUI/ServerForm.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO.Pipes;
+using System.IO;
+using System.Diagnostics;
+
+namespace Server_GUI
+{
+    public partial class ServerForm : Form
+    {
+        delegate void Engine_Activation_Delegate();
+
+        
+     
+        public ServerForm()
+        {
+            InitializeComponent();
+            this.Text = "Server Window";
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            Thread engine = new Thread(new ThreadStart(Pipe_Reader));
+            engine.Start();
+
+        }
+
+        public void Pipe_Reader()
+        {
+            // Open the named pipe.
+            var server = new NamedPipeServerStream("Data");
+
+            if (this.InvokeRequired)
+            {
+                Engine_Activation_Delegate Engine_Activation = new Engine_Activation_Delegate(this.Engine_Activation);
+                this.Invoke(Engine_Activation);
+            }
+
+            //Console.WriteLine("Waiting for connection...");
+            server.WaitForConnection();
+
+
+            //Console.WriteLine("Connected.");
+            var br = new BinaryReader(server);
+            //var bw = new BinaryWriter(server);
+            int i = 0;
+            while (true)
+            {
+                try
+                {
+                    var len = (int)br.ReadUInt32();
+                    if (len == 0)
+                        MessageBox.Show("len equals zero");
+
+                    var str = new string(br.ReadChars(len));    // Read string
+                    if (i == 0)
+                    {
+                        //if (this.InvokeRequired)
+                        //{
+                        //    this.Invoke((MethodInvoker)delegate
+                        //    {
+                        //        MessageBox.Show(str);
+                        //    });
+                            
+                        //}
+                       
+                    }
+                    if (i == 1)
+                    {
+                      
+                        //if (this.InvokeRequired)
+                        //{
+
+                        //    this.Invoke((MethodInvoker)delegate
+                        //    {
+                        //        MessageBox.Show(str);
+                        //    });
+                            
+                        //}
+                        Pipe_Writer(server);
+                        i = 0;
+                    }
+                      i++;
+                }
+
+                catch
+                {
+                    //MessageBox.Show("Error");
+                }
+
+               
+            }
+
+            
+        }
+
+
+
+        public void Pipe_Writer(NamedPipeServerStream server)
+        {
+            string Data_To_Send = "Message From Server Using Pipe";
+
+            if (this.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    textBox2.Text += Data_To_Send;
+
+                });
+            }
+            var bw = new BinaryWriter(server);
+            var buf = Encoding.ASCII.GetBytes(Data_To_Send);                // Get ASCII byte array     
+            bw.Write((uint)buf.Length);                                     // Write string length
+            bw.Write(buf);   
+        }
+
+
+        public void Engine_Activation()
+        {
+            Process pythonProcess = new Process();
+            pythonProcess.StartInfo.FileName = @"C:\Python27\python.exe";
+            pythonProcess.StartInfo.Arguments = @"C:\Users\User\Desktop\22-2-16CP\Server_Side\Server_GUI\bin\Debug\Python\Project_Engine.py";
+            pythonProcess.StartInfo.WorkingDirectory = Application.StartupPath;
+            pythonProcess.Start();     
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    
+
+  
+    }
+
+
+}
+
